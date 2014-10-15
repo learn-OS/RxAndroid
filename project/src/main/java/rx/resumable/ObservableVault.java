0e37f126638b95f03402f998b70eb3b48a7b36bc
Dorvaryn <benjamin@odai.homelinux.net>
2014-10-15 11:59:43 +0100

    Adding javadoc

--- project/src/main/java/rx/resumable/ObservableVault.java
+++ project/src/main/java/rx/resumable/ObservableVault.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2014 Novoda, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package rx.resumable;
 
 import java.util.Collections;
@@ -6,18 +21,43 @@ import java.util.Map;
 
 import rx.Observable;
 
+/**
+ * This is a Storage Object that will retain Observables based on two keys:
+ * - ResumableReference with the resumableId: identifying the object starting observables (ie: HomeActivity, SomeSpecificFragment, etc)
+ * - id: identifying a specific Observable in this reference (ie: fetchData, someObservable, etc)
+ *
+ * This Object being the holder will usually sit at Application level.
+ */
 public class ObservableVault {
 
     private final Map<Integer, Map<Integer, Observable>> arraysOfObservables = new HashMap<Integer, Map<Integer, Observable>>();
 
+    /**
+     * Store the given Observable using given reference and id
+     * @param reference Reference of the class storing this observable
+     * @param id id of the Observable to store
+     * @param observable the observable to store
+     * @return previous observable if one was already stored with this id and reference
+     */
     public synchronized Observable put(ResumableReference reference, int id, Observable observable) {
         return getObservablesFor(reference).put(id, observable);
     }
 
+    /**
+     * Remove the given Observable using given reference and id
+     * @param reference Reference of the class storing this observable
+     * @param id id of the Observable to remove
+     * @return Observable that was removed from the Vault
+     */
     public synchronized Observable remove(ResumableReference reference, int id) {
         return getObservablesFor(reference).remove(id);
     }
 
+    /**
+     * Get all the Observables stored for a given reference
+     * @param reference Reference of the class storing the observables
+     * @return A map of Key/Observables stored for the given reference
+     */
     public synchronized Map<Integer, Observable> getImmutableObservablesFor(ResumableReference reference) {
         return Collections.unmodifiableMap(new HashMap<Integer, Observable>(getObservablesFor(reference)));
     }
--- project/src/main/java/rx/resumable/ResumableSubscriber.java
+++ project/src/main/java/rx/resumable/ResumableSubscriber.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2014 Novoda, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package rx.resumable;
 
 import java.util.ArrayList;
@@ -13,6 +28,14 @@ import rx.resumable.operators.EventCachingOperator;
 import rx.resumable.operators.EventForwardingListener;
 import rx.resumable.subject.ForwardingSubject;
 
+/**
+ * (Might need a new name since it is confusing now that RxJava uses Subscriber for the wrapper around Observer)
+ *
+ * This Object is performing the subscription to Observables as a proxy
+ * The aim is to handle storage of Observables and recreate and reattach Observers upon reference lifecycle changes
+ *
+ * On Android this object would sit at Activity/Fragment level
+ */
 public class ResumableSubscriber {
 
     private final List<SubscriptionWithId> subscriptions = new ArrayList<SubscriptionWithId>();
@@ -20,12 +43,17 @@ public class ResumableSubscriber {
     private final ObservableVault observableVault;
     private final ObserverFactory factory;
 
+
     public ResumableSubscriber(ResumableReference resumableReference, ObserverFactory factory, ObservableVault observableVault) {
         this.resumableReference = resumableReference;
         this.observableVault = observableVault;
         this.factory = factory;
     }
 
+    /**
+     * Recreate and reattach Observers for the current resumableReference
+     * On Android would be call in onResume
+     */
     public void resume() {
         Map<Integer, Observable> observableMap = observableVault.getImmutableObservablesFor(resumableReference);
         for (Map.Entry<Integer, Observable> observableEntry : observableMap.entrySet()) {
@@ -36,6 +64,10 @@ public class ResumableSubscriber {
         }
     }
 
+    /**
+     * Unsubscribe all current Observers to avoid leaking the current reference
+     * On Android would be called in onPause
+     */
     public void pause() {
         for (Subscription subscription : subscriptions) {
             subscription.unsubscribe();
@@ -43,17 +75,37 @@ public class ResumableSubscriber {
         subscriptions.clear();
     }
 
+    /**
+     * Subscribes the given observer to the observable and cache events emitted by the observables between calls to pause and resume
+     * @param observable The observable to subscribe to
+     * @param observer The Observer we want to subscribe
+     * @param <T> The type of the Observable
+     */
     public <T> void subscribe(Observable<? extends T> observable, ResumableObserver<T> observer) {
         ForwardingSubject<T> proxy = new ForwardingSubject<T>(new EventCachingOperator<T>(new CleanObservable(observer.getId())));
         observable.subscribe(proxy);
         subscribe(observer, proxy);
     }
 
+    /**
+     * Subscribes the given observer to the observable and replay all events upon resume
+     *
+     * @param observable The observable to subscribe to
+     * @param observer   The Observer we want to subscribe
+     * @param <T>        The type of the Observable
+     */
     public <T> void subscribeWithReplay(Observable<? extends T> observable, ResumableObserver<T> observer) {
         Observable<? extends T> replayEnabled = observable.cache();
         subscribe(observer, replayEnabled);
     }
 
+    /**
+     * Subscribes the given observer to the observable and drop events emitted by the observables between calls to pause and resume
+     *
+     * @param observable The observable to subscribe to
+     * @param observer   The Observer we want to subscribe
+     * @param <T>        The type of the Observable
+     */
     public <T> void subscribeWithDrop(Observable<? extends T> observable, ResumableObserver<T> observer) {
         ForwardingSubject<T> proxy = new ForwardingSubject<T>(new DropEventOperator<T>(new CleanObservable(observer.getId())));
         observable.subscribe(proxy);
@@ -66,6 +118,10 @@ public class ResumableSubscriber {
         subscriptions.add(subscription);
     }
 
+    /**
+     * Unsubscribe the given observer
+     * @param observerId id of the observer to unsubscribe.
+     */
     public void unsubscribe(final int observerId) {
         for (Iterator<SubscriptionWithId> iterator = subscriptions.iterator(); iterator.hasNext(); ) {
             SubscriptionWithId subscription = iterator.next();
